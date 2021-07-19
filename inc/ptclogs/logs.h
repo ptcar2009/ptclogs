bc7eec0f1b09fabf1bae79fb92a6478285838e89
Pedro Tavares de Carvalho <pcarvalho@cyral.com>
2021-07-18 22:15:35 -0300

    dev(docs): redoes documentation for logs.h

--- inc/ptclogs/logs.h
+++ inc/ptclogs/logs.h
@@ -14,179 +14,172 @@ namespace logger {
 template <class Driver>
 class Logger {
  public:
-  
   /**
-   * @brief Prints an object with the log level WARN.
+   * @brief Logs the object t at WARN log level.
    *
-   * @tparam T Type of the object that will be printed.
-   * @param t Object that will be printed.
+   * @tparam T Type of the object to be printed.
+   * @param t Object to be printed.
    */
   template <typename T>
   void WARN(T t) {
-    if (verbosity < LogLevel::WARN) return;
+    if (log_level < LogLevel::WARN) return;
     print_object(t, LogLevel::WARN);
   }
+
   /**
-   * @brief Prints objects, separated by spaces, at the log level WARN and then
-   * exits with an error.
+   * @brief Logs the message with its fields at WARN log level.
    *
-   * @tparam T Type of the first object to be printed.
-   * @tparam Args Type of the other objecs.
-   * @param t First object to be printed.
-   * @param args Other objects to be printed.
+   * @tparam Args Types of the fields that will be printed.
+   * @param message Message that will be printed.
+   * @param args Fields that will be printed alongside the message.
    */
   template <typename... Args>
   void WARN(std::string message, Field<Args>... args) {
-    if (verbosity < LogLevel::WARN) return;
+    if (log_level < LogLevel::WARN) return;
     print_message(message, LogLevel::WARN, args...);
   }
+
   /**
-   * @brief Prints an object with the log level FATAL and then exits with an
-   * error.
+   * @brief Logs the object t at FATAL log level and calls exit(1).
    *
    * @tparam T Type of the object that will be printed.
    * @param t Object that will be printed.
    */
   template <typename T>
   void FATAL(T t) {
-    if (verbosity < LogLevel::FATAL) return;
+    if (log_level < LogLevel::FATAL) return;
     print_object(t, LogLevel::FATAL);
     exit(1);
   }
+
   /**
-   * @brief Prints objects, separated by spaces, at the log level FATAL and then
-   * exits with an error.
+   * @brief Logs the message with its fields at FATAL log level and calls
+   * exit(1).
    *
-   * @tparam T Type of the first object to be printed.
-   * @tparam Args Type of the other objecs.
-   * @param t First object to be printed.
-   * @param args Other objects to be printed.
+   * @tparam Args Types of the fields that will be printed.
+   * @param message Message that will be printed.
+   * @param args Fields that will be printed alongside the message.
    */
   template <typename... Args>
   void FATAL(std::string message, Field<Args>... args) {
-    if (verbosity < LogLevel::FATAL) return;
+    if (log_level < LogLevel::FATAL) return;
     print_message(message, LogLevel::FATAL, args...);
     exit(1);
   }
 
   /**
-   * @brief Prints an object with the log level ERROR and then exits with an
-   * error.
+   * @brief Logs the object t at ERROR log level.
    *
-   * @tparam T Type of the object that will be printed.
-   * @param t Object that will be printed.
+   * @tparam T Type of the object to be printed.
+   * @param t Object to be printed.
    */
   template <typename T>
   void ERROR(T t) {
-    if (verbosity < LogLevel::ERROR) return;
+    if (log_level < LogLevel::ERROR) return;
     print_object(t, LogLevel::ERROR);
   }
+
   /**
-   * @brief Prints objects, separated by spaces, at the log level ERROR and then
-   * exits with an error.
+   * @brief Logs the message with its fields at ERROR log level.
    *
-   * @tparam T Type of the first object to be printed.
-   * @tparam Args Type of the other objecs.
-   * @param t First object to be printed.
-   * @param args Other objects to be printed.
+   * @tparam Args Types of the fields that will be printed.
+   * @param message Message that will be printed.
+   * @param args Fields that will be printed alongside the message.
    */
   template <typename... Args>
   void ERROR(std::string message, Field<Args>... args) {
-    if (verbosity < LogLevel::ERROR) return;
+    if (log_level < LogLevel::ERROR) return;
     print_message(message, LogLevel::ERROR, args...);
   }
 
   /**
-   * @brief Prints an object with the log level INFO.
+   * @brief Logs the object t at INFO log level.
    *
-   * @tparam T Type of the object that will be printed.
-   * @param t Object that will be printed.
+   * @tparam T Type of the object to be printed.
+   * @param t Object to be printed.
    */
   template <typename T>
   void INFO(T t) {
-    if (verbosity < LogLevel::INFO) return;
+    if (log_level < LogLevel::INFO) return;
     print_object(t, LogLevel::INFO);
   }
 
   /**
-   * @brief Prints objects, separated by spaces, at the log level INFO.
+   * @brief Logs the message with its fields at INFO log level.
    *
-   * @tparam T Type of the first object to be printed.
-   * @tparam Args Type of the other objecs.
-   * @param t First object to be printed.
-   * @param args Other objects to be printed.
+   * @tparam Args Types of the fields that will be printed.
+   * @param message Message that will be printed.
+   * @param args Fields that will be printed alongside the message.
    */
   template <typename... Args>
   void INFO(std::string message, Field<Args>... args) {
-    if (verbosity < LogLevel::INFO) return;
+    if (log_level < LogLevel::INFO) return;
     print_message(message, LogLevel::INFO, args...);
   }
 
   /**
-   * @brief Prints an object with the log level INFO.
+   * @brief Logs the object t at DEBUG log level.
    *
-   * @tparam T Type of the object that will be printed.
-   * @param t Object that will be printed.
+   * @tparam T Type of the object to be printed.
+   * @param t Object to be printed.
    */
   template <typename T>
   void DEBUG(T t) {
-    if (verbosity < LogLevel::DEBUG) return;
+    if (log_level < LogLevel::DEBUG) return;
     print_object(t, LogLevel::DEBUG);
   }
 
   /**
-   * @brief Prints objects, separated by spaces, at the log level DEBUG.
+   * @brief Logs the message with its fields at DEBUG log level.
    *
-   * @tparam T Type of the first object to be printed.
-   * @tparam Args Type of the other objecs.
-   * @param t First object to be printed.
-   * @param args Other objects to be printed.
+   * @tparam Args Types of the fields that will be printed.
+   * @param message Message that will be printed.
+   * @param args Fields that will be printed alongside the message.
    */
   template <typename... Args>
   void DEBUG(std::string message, Field<Args>... args) {
-    if (verbosity < LogLevel::DEBUG) return;
+    if (log_level < LogLevel::DEBUG) return;
     print_message(message, LogLevel::DEBUG, args...);
   }
 
   /**
    * @brief Sets the log level of the logger.
    *
-   * @param verbosity Log level that will be set.
+   * @param log_level Log level that will be set.
    */
-  void SetLogLevel(LogLevel verbosity) { this->verbosity = verbosity; };
+  void SetLogLevel(LogLevel log_level) { this->log_level = log_level; };
 
   /**
    * @brief Returns the current log level of the logger.
    *
    * @return Log level of the logger.
    */
-  LogLevel GetLogLevel() { return verbosity; };
+  LogLevel GetLogLevel() { return log_level; };
 
   /**
-   * @brief Instantiates a logger based on the environment variable "VERBOSITY".
-   * The variable should be a number between 0 and 2 both inclusive,
-   * representing ERROR, INFO and DEBUG, respectively.
+   * @brief Instantiates a logger with the log level from the VERBOSITY env var.
    *
+   *
+   * @param out Output stream for the logger.
    */
-  Logger(std::ostream& out = std::cout) : out(out),
-                                               driver(out, verbosity) {
-    verbosity = LogLevel::INFO;
+  Logger(std::ostream& out = std::cout) : out(out), driver(out, log_level) {
+    log_level = LogLevel::INFO;
     if (getenv("VERBOSITY") != NULL)
-      verbosity = LogLevel(atoi(getenv("VERBOSITY")));
+      log_level = LogLevel(atoi(getenv("VERBOSITY")));
   };
 
   /**
    * @brief Instantiates a logger with the given log level that writes on `out`.
    *
-   * @param verbosity Log level of the logger.
+   * @param log_level Log level of the logger.
    * @param out Output stream on which the logger will write to.
    */
-  Logger(LogLevel verbosity, std::ostream& out = std::cout)
-      : verbosity(verbosity), driver(out, verbosity), out(out){};
+  Logger(LogLevel log_level, std::ostream& out = std::cout)
+      : log_level(log_level), driver(out, log_level), out(out){};
 
  private:
   Driver driver;
-  LogLevel verbosity;
+  LogLevel log_level;
   template <typename T>
   void printv(Field<T> field) {
     driver.print_field(field.header, field.value);
@@ -213,6 +206,7 @@ class Logger {
     driver.end_message();
     out << std::endl;
   }
+
   template <typename... Args>
   void print_message(std::string message, LogLevel level, Field<Args>... args) {
     driver.begin_message();
