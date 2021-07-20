11a54ba17b4b99909ccdb0982b6ca3700027e710
Pedro Tavares de Carvalho <pcarvalho@cyral.com>
2021-07-20 17:07:24 -0300

    dev(fields): changes trace to not have lambda function

--- inc/ptclogs/fields.hpp
+++ inc/ptclogs/fields.hpp
@@ -2,12 +2,10 @@
 #define PTCLOGS_FIELDS_HPP
 #include <sstream>
 
-#define ptclogs_trace()                      \
-  logger::Field<std::string>("trace", []() { \
-    std::stringstream strim;                 \
-    strim << __FILE__ << ":" << __LINE__;    \
-    return strim.str();                      \
-  }())
+#define ptclogs_trace()       \
+  logger::Field<std::string>( \
+      "trace",                \
+      std::string(__FILE__) + ":" + std::string(std::to_string(__LINE__)))
 
 #define ptclogs_caller() \
   logger::Field<std::string>("caller", __PRETTY_FUNCTION__)
